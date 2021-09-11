dc79882b1d84a2d4aee5aa912316bd4f074eda45
Luiz Felipe <lfsprj@gmail.com>
2021-09-10 23:22:51 -0300

    added contact section and slider content, also added scroll reveal.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const menu = document.querySelectorAll('nav ul li a')
 
-console.log(menu)
 
 for (const element of toggle) {
     element.addEventListener('click', function() {
@@ -29,3 +28,34 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll')
     }
 })
+
+
+/*slider*/
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+
+});
+
+/*Scroll reveal*/
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(`
+#home .text, #home .image,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links
+
+`, {interval: 100})
